1417254982753bc6322f44407a13216a9000e567
eohbell05 <G00466435@atu.ie>
2026-01-28 15:07:40 +0000

    updated comments

--- ex2.ts
+++
@@ -1,9 +0,0 @@
-//C:\Users\EthanOHalloranBell-S\OneDrive - Atlantic TU\Year 2\Front end web development\Lab1>C:\Users\EthanOHalloranBell-S\Desktop\node-v24.13.0-win-x64\node.exe ex2.ts
-function addition(value: string) {
-    console.log("Value is: " + value);
-}
-
-let firstVal: number = 42;
-let secondVal: number = 1;
-let sumOfVals: string = (firstVal + secondVal).toLocaleString();
-addition(sumOfVals);
